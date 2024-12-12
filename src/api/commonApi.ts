5bb4a848ab1c2481c7a4550513d37b19a73e4650
Olga Genier <genier.developer@gmail.com>
2024-12-12 17:08:29 +0500

    refactor

--- src/api/commonApi.ts
+++ src/api/commonApi.ts
@@ -3,7 +3,7 @@ import { ResultCode } from 'api/profileApi'
 
 export const instance = axios.create({
     withCredentials: true,
-    headers: { 'API-KEY': 'daee8b0a-f79f-4c77-a8a2-1e2410d2bc56' },
+    headers: { 'API-KEY': '33748f50-4732-4375-bd5e-491763a6867d' },
     baseURL: 'https://social-network.samuraijs.com/api/1.0/'
 })
 
--- src/pages/profilePage/myFriends/MyFriends.tsx
+++ src/pages/profilePage/myFriends/MyFriends.tsx
@@ -1,14 +1,27 @@
-import { useEffect, useMemo } from 'react'
+import { Typography } from 'components/common'
+import s from './MyFriends.module.css'
+import { useSelector } from 'react-redux'
+import { usersSelector } from 'pages/usersPage'
+import { useEffect } from 'react'
+import { getUsersTC } from 'redux/usersReducer'
+import { useAppDispatch } from 'redux/store'
+import { Friend } from 'pages/profilePage/myFriends/friend/Friend'
 
-export const MyFriends = ({ isActive }: { isActive: boolean }) => {
-    const filter = useMemo(() => ({
-        isFriend: true,
-        isActive,
-    }), [isActive])
+export const MyFriends = () => {
+    const friends = useSelector(usersSelector)
+    const filter = { term: '', friend: true }
+    const dispatch = useAppDispatch()
 
     useEffect(() => {
-        console.log(filter)
-    }, [filter])
+        dispatch(getUsersTC(1, 100, filter))
+    }, [dispatch, filter])
 
-    return <div>My Friends</div>
+    return (
+        <div className={s.wrapper}>
+            <Typography variant={'h3'}> My friends</Typography>
+            <div className={s.friendsWrapper}>
+                {friends.map(friend => <Friend key={friend.id} friend={friend} />)}
+            </div>
+        </div>
+    )
 }
