bb7d12de84e62de53b439e18dadd6e923691588c
Olga Genier <genier.developer@gmail.com>
2024-12-12 16:44:07 +0500

    feat: memoization refreshProfile

--- src/pages/profilePage/ProfilePage.tsx
+++ src/pages/profilePage/ProfilePage.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react'
+import { useCallback, useEffect } from 'react'
 import { Profile } from 'pages/profilePage/profile'
 import { useSelector } from 'react-redux'
 import { getProfile, getStatus } from 'redux/profileReducer'
@@ -12,32 +12,35 @@ type PathParams = {
     userId?: string | undefined
 }
 
-export const ProfilePage = () => {
 
+
+export const ProfilePage = () => {
     const authorizedUserId = useSelector(authorizedUserIdSelector)
     const { userId } = useParams<PathParams>()
     const dispatch = useAppDispatch()
 
     const meId = String(authorizedUserId)
 
-    let id = userId ? Number(userId) : authorizedUserId
+    const id = userId ? Number(userId) : authorizedUserId
 
-    const refreshProfile = async () => {
+    const refreshProfile = useCallback(async () => {
         if (id) {
             dispatch(getProfile(id))
             dispatch(getStatus(id))
         }
-    }
+    }, [id, dispatch])
 
     useEffect(() => {
-        refreshProfile().then()
-    }, [userId, refreshProfile])
+        refreshProfile()
+    }, [refreshProfile])
 
-    let showFriends = id && meId && (String(id) === meId)
+    const showFriends = id && meId && String(id) === meId
 
     return (
         <section>
-            <Typography variant={'h2'} as={'h2'}>Profile</Typography>
+            <Typography variant={'h2'} as={'h2'}>
+                Profile
+            </Typography>
             <Profile isOwner={!userId} />
             {showFriends && <MyFriends />}
         </section>
